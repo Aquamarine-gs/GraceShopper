11de529f6484797606435539c3f5eced826a2b53
yajessicachen <yajessicachen@gmail.com>
2022-04-19 14:06:23 -0400

    added user and product models

---
+++ server/db/models/Product.js
@@ -0,0 +1,34 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Product = db.define('product', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  price: {
+    type: Sequelize.DECIMAL(10, 2),
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  description: {
+    type: Sequelize.TEXT,
+  },
+  imageUrl: {
+    type: Sequelize.TEXT,
+    defaultValue:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Pok%C3%A9_Ball_icon.svg/640px-Pok%C3%A9_Ball_icon.svg.png',
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+  },
+  category: {
+    type: Sequelize.STRING,
+  },
+})
+module.exports = Product
