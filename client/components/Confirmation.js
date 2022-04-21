c30e58f5c21950cf9150abc16c1d384a0aaced79
yajessicachen <yajessicachen@gmail.com>
2022-04-21 12:38:44 -0400

    fixed typo

--- client/components/Confirmation.js
+++ client/components/Confirmation.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 export const Confirmation = () => {
   return (
     <div>
-      <h1>COMFIRMATION</h1>
+      <h1>CONFIRMATION</h1>
       <h2>PURCHASE SUCCESSFUL!</h2>
-      <p>Your receipt has been emailed to you</p>
+      <p>Your receipt has been emailed to you.</p>
       <img src="https://perdidointranslation.files.wordpress.com/2016/07/pokemon-happy.gif" />
       <br />
       <Link to>Back to Home Page</Link>
