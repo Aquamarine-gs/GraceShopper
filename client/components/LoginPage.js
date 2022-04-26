9ab77e6049a58f43ebbde407c055b980b91e3cd6
yajessicachen <yajessicachen@gmail.com>
2022-04-26 11:12:06 -0400

    style for login page

--- client/components/LoginPage.js
+++ client/components/LoginPage.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { login } from '../store/auth';
 import history from '../history';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 const LoginPage = () => {
   const [email, setEmail] = useState('');
@@ -27,24 +29,37 @@ const LoginPage = () => {
   };
 
   return (
-    <div>
-      <h1>LoginPage</h1>
-      <form onSubmit={onSubmit}>
-        <input
+    <Form className="text-center" onSubmit={onSubmit}>
+      <h1>Login Page</h1>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control
           type="email"
-          name="email"
+          placeholder="Enter email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
-        <input
+
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+      <br />
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
           type="password"
-          name="password"
+          placeholder="Password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
   );
 };
 
